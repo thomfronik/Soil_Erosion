ca835afbf31d879a6e0c9a8b6960da9caf738478
Thom <thom.fronik@gmail.com>
2019-02-19 17:38:50 +0000

    Bounds for eta as functions of cr added

--- numerical_scheme.m
+++ numerical_scheme.m
@@ -1,8 +1,10 @@
 % Numerical scheme for solving the coupled system of equations for h and c
 % Considers one particle size only 
+
 clear, close all
+
 % Initialise parameters 
-eta = 1; 
+cr = 1; 
 
 % Initialise constants and scaling factors
 g = 9.81; 
@@ -16,10 +18,12 @@ delta = (x0*q0^2*n^2)/(h0^(13/3));
 % Initial values
 xr = 0;
 hr = (q0^2/(g*h0^3))^(1/3);
-cr_upper = eta*delta*(1-1/(hr^(13/3)))+1/(hr^(13/3)); 
-cr_lower = 1/(hr^(13/3))-1/(108*delta*eta*hr^(29/3))*(10*delta*eta*hr-13)^2; 
-cr = 0.45;
+%cr_upper = eta*delta*(1-1/(hr^(13/3)))+1/(hr^(13/3)); 
+%cr_lower = 1/(hr^(13/3))-(1/(108*eta*hr^(29/3)))*(10*delta*eta*hr-13)^2; 
 
+eta_lower = (cr-1/(hr^(13/3)))/(delta*(1-1/(hr^(13/3)))); 
+eta_upper = fsolve(@(x) 1/(hr^(13/3))-1/(108*x*hr^(29/3))*(10*x*delta*hr-13)^2 - cr, eta_lower);
+eta = eta_lower; 
 %%
 % Here we insert our analytical solution around hr
 % For now its just an ugly perturbation
@@ -34,7 +38,7 @@ h_expansion = @(x) hr + b0/(hr^(13/3)*eta)*(x-xr)+b0*b1/(2*hr^(29/3)*eta^2)*(x-x
 
 % Boundaries of analytical solution
 M = 10;
-epsilon = 10^-3;
+epsilon = 10^-1;
 xr_plus = xr + epsilon;
 xr_min = xr - epsilon; 
 x_M = linspace(xr_min, xr_plus, M); 
@@ -63,8 +67,8 @@ x_R = linspace(xr_plus, x_max, N);
 x_L = linspace(xr_min, x_min, N);
 
 % Solving the coupled system of equations using ODE45
-[x_R, y_R] = ode45(@(t,x) system_one_particle_size(x, t, hr, eta, delta), x_R , y0_R);
-[x_L, y_L] = ode45(@(t,x) system_one_particle_size(x, t, hr, eta, delta), x_L, y0_L); 
+[x_R, y_R] = ode23s(@(t,x) system_one_particle_size(x, t, hr, eta, delta), x_R , y0_R);
+[x_L, y_L] = ode23s(@(t,x) system_one_particle_size(x, t, hr, eta, delta), x_L, y0_L); 
 
 % Plotting the piecewise solutions
 figure
@@ -72,6 +76,7 @@ hold on
 plot(x_L, real(y_L(:,1)))
 plot(x_M, real(h_M))
 plot(x_R, real(y_R(:,1)))
+hold off
 xlabel('$\xi$', 'Interpreter', 'latex') 
 ylabel('h($\xi$)', 'Interpreter', 'latex') 
 
@@ -79,6 +84,7 @@ figure
 hold on 
 plot(x_L, real(y_L(:,2)))
 plot(x_R, real(y_R(:,2)))
+hold off
 xlabel('$\xi$', 'Interpreter', 'latex') 
 ylabel('c($\xi$)', 'Interpreter', 'latex') 
 
