efe75292ee2d32bba2cf6647fb6d79e7137479fb
Thom <thom.fronik@gmail.com>
2019-02-19 01:25:05 +0000

    Function for the ODE system

---
+++ system_one_particle_size.m
@@ -0,0 +1,7 @@
+function y = system_one_particle_size(x,t,hr, eta, delta)
+
+    h = -((x(1).^3-hr^3)^(-1))*(x(1).^3.*((1/eta)*(x(2)-1./(x(1).^(13/3))) - delta) + delta./(x(1).^(13/3)));
+    c = 1./(x(1).^(13/3))-x(2); 
+    y = [h ; c];
+
+end
