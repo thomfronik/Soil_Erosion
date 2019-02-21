d58c3a691e37294e84074f939395dcf3e7d2f957
Vincent Meijer <vincentmeijer@Vincents-MacBook-Pro.local>
2019-02-21 17:55:27 +0000

    Made function that finds xi_min, c_min based on RH conditions, now correct

--- cmin_finder.m
+++ cmin_finder.m
@@ -1,3 +1,3 @@
 function cmin = cmin_finder(h_spline, c_spline, hplus, options)
-xi_min = fsolve(@(x) ppval(h_spline,x) - hplus, zeros(1,length(hplus)), options); 
+xi_min = fsolve(@(x) ppval(h_spline,x) - hplus, zeros(size(hplus)), options); 
 cmin= ppval(c_spline, xi_min); 
