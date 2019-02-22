0d6e6c1e82fee6bc60e8931cd3fa94d9d28bf682
Thom <thom.fronik@gmail.com>
2019-02-22 15:44:14 +0000

    right scaling added

--- error_function.m
+++ error_function.m
@@ -1,7 +1,7 @@
 function err = error_function(c_spline, xmin, xplus, int_points) 
 
     err_c_condition = (ppval(c_spline, xmin) - ppval(c_spline, xplus))^2;
-    err_int_condition = (1-trapz(ppval(c_spline, linspace(xmin, xplus, int_points))))^2; 
+    err_int_condition = (1-(1/abs(xmin - xplus))*trapz(ppval(c_spline, linspace(xmin, xplus, int_points))))^2; 
     err = err_c_condition + err_int_condition;
 
 end
