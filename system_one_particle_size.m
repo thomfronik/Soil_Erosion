377a4ba85086cba4cfe9b1365ce30f51a92cb48e
Thom <thom.fronik@gmail.com>
2019-02-24 21:29:20 +0000

    Corrected a typo

--- system_one_particle_size.m
+++ system_one_particle_size.m
@@ -1,6 +1,6 @@
 function y = system_one_particle_size(x,t,hr, eta, delta)
 
-    h = -((x(1).^3-hr^3)^(-1))*(x(1).^3.*((1/eta)*(x(2)-1./(x(1).^(13/3))) - delta) + delta./(x(1).^(13/3)));
+    h = -((x(1).^3-hr^3)^(-1))*(x(1).^3.*((1/eta)*(x(2)-1./(x(1).^(13/3))) - delta) + delta./(x(1).^(1/3)));
     c = 1./(x(1).^(13/3))-x(2); 
     y = [h ; c];
 
